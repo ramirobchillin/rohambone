10bbe9204b558c31de0b3fd445abf7807ffb2e39
ramirobchillin <ramirobc2d@outlook.com>
2020-07-19 09:49:50 -0400

    Created shoot function

--- RoHamBone.py
+++ RoHamBone.py
@@ -5,3 +5,7 @@ class Player:
     def __init__(self, Name):
         self.Name = Name 
         self.move = None 
+    
+    def shoot(self):
+        possible_moves = ['Rock', 'Paper', 'Scissors']
+        return choice(possible_moves) 
